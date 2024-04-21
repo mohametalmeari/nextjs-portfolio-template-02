ef6e7352f928db9cd25f30d8910324c0b617fbf8
mohametalmeari <mohamet.almeari@gmail.com>
2024-04-21 14:58:04 +0300

    Add AboutSection to home page

--- src/app/(routes)/page.js
+++ src/app/(routes)/page.js
@@ -1,9 +1,11 @@
+import AboutSection from "../components/AboutSection";
 import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-[#161513]">
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,49 @@
+import { Button } from "@/components/ui/button";
+import { PaperclipIcon } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+const AboutSection = () => {
+  return (
+    <section className="md:px-20 lg:px-40 w-full flex justify-between mb-20">
+      <div className="flex-1 flex justify-center items-center">
+        <div className="bg-gradient-to-br from-[#b16cea]  via-[#ff5e69] to-[#ffa84b] rounded-t-full overflow-hidden">
+          <Image src={"/hero-image.png"} width={300} height={500} />
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col gap-10">
+        <h2 className="text-6xl text-transparent bg-clip-text bg-gradient-to-r from-[#b16cea] via-[#ff5e69] to-[#ffa84b] place-self-start font-semibold">
+          About Me
+        </h2>
+        <p className="text-sm text-gray-300 leading-7">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+          minus impedit ipsa sint architecto quisquam optio dolorem nihil
+          aspernatur quis ut eos voluptatum debitis similique mollitia quibusdam
+          repellat itaque placeat.
+        </p>
+        <div className="flex gap-5 justify-between">
+          <Achievement number={10} text="Lorem ipsum dolor sit amet." />
+          <Achievement
+            number={120}
+            text="Lorem ipsum dolor sit amet sit amet."
+          />
+          <Achievement number={60} text="Lorem ipsum dolor." />
+        </div>
+        <Button className="place-self-start flex gap-2">
+          Download CV <PaperclipIcon />
+        </Button>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
+
+const Achievement = ({ number, text }) => (
+  <div className="flex flex-col gap-5 flex-1 max-w-36">
+    <span className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-[#b16cea] via-[#ff5e69] to-[#ffa84b] place-self-start font-semibold">
+      {number}+
+    </span>
+    <span className="text-sm text-gray-300 leading-7">{text}</span>
+  </div>
+);
