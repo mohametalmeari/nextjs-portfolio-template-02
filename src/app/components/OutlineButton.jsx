3d2b69714a1b409887c6ddd66efda631ee6d1b79
mohametalmeari <mohamet.almeari@gmail.com>
2024-04-21 14:23:54 +0300

    Create customized buttons

---
+++ src/app/components/OutlineButton.jsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const OutlineButton = ({ children, className, onClick }) => {
+  return (
+    <Button
+      className={cn("p-[1px] flex items-stretch justify-stretch", className)}
+      variant="outline"
+      onClick={onClick}
+    >
+      <span className="bg-[#161512] flex items-center justify-center rounded-md px-4 py-2">
+        {children}
+      </span>
+    </Button>
+  );
+};
+
+export default OutlineButton;
