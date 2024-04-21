8720c487253288ca7a08980969fe2a2bbc3a5d78
mohametalmeari <mohamet.almeari@gmail.com>
2024-04-21 18:28:07 +0300

    Add QualificationsSection to about page

--- src/app/(routes)/about/page.js
+++ src/app/(routes)/about/page.js
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection";
+import QualificationsSection from "./components/QualificationsSection";
 
 export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-[#161513]">
       <HeroSection />
+      <QualificationsSection />
     </main>
   );
 }
